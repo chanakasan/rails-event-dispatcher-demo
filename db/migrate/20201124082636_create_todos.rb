733abcce68f94c5d3c3848c3b3a74e598c7229c6
Chanaka Abeysinghe <chanakasan@gmail.com>
2020-11-24 13:59:08 +0530

    scaffold todos

---
+++ db/migrate/20201124082636_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todos do |t|
+      t.string :name, null: false
+      t.boolean :active, default: true, null: false
+
+      t.timestamps
+    end
+  end
+end
